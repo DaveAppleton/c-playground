738b28e5a6d00b36097cf25cacdd42ed07101864
Dave Appleton <calistralabs@gmail.com>
2022-07-06 22:02:12 +0800

    get a number

--- test-sscanf/testsscanf.c
+++ test-sscanf/testsscanf.c
@@ -5,7 +5,13 @@
 int main() {
     char line[1024];
     char part1[25],part2[25],part3[25],part4[25];
-    strcpy(line,"hello how are you\n");
+    int num;
+    strcpy(line,"hello how are 356\n");
     sscanf(line,"%s %s %s %s\n",part1,part2,part3,part4);
     printf("* %s\n* %s\n* %s\n* %s\n",part1,part2,part3,part4);
+    sscanf(part4,"%d",&num);
+    printf("%d\n",num* 2);
+
+
+
 }
